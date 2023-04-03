5cd886f936ecadeef0eaac06a9d21e4bac8523ff
daancabo97 <danielcaicedo285@gmail.com>
2023-04-02 20:41:35 -0500

    validando conexion cliente y servidor 4

--- src/com/arquitectura/net/server/ServerThread.java
+++ src/com/arquitectura/net/server/ServerThread.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ServerThread extends Thread {
 
@@ -24,13 +25,19 @@ public class ServerThread extends Thread {
             output = new PrintWriter(socket.getOutputStream(), true);
 
             // Leer los mensajes del cliente y enviarles una respuesta
-            while (true) {
+            while (!socket.isClosed()) {
                 String message = input.readLine();
                 if (message == null) {
                     break;
                 }
                 System.out.println("Mensaje recibido del cliente: " + message);
                 output.println("Respuesta del servidor: " + message);
+            }
+        } catch (SocketException e) {
+            if (e.getMessage().equals("Connection reset")) {
+                System.out.println("Conexión cerrada por el cliente");
+            } else {
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
